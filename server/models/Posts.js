764fae0aac9418afa558a0eaa6e9b2c6da9638b3
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2019-02-24 17:45:52 -0300

    add post model schema and setup react

--- server/models/Posts.js
+++ server/models/Posts.js
@@ -7,6 +7,50 @@ const postSchema = new Schema({
         type: Schema.Types.ObjectId,
         ref: 'users'
     },
+    text: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String
+    },
+    avatar: {
+        type: String
+    },
+    likes: [
+        {
+            user: {
+                type: Schema.Types.ObjectId,
+                ref: 'users'
+            }
+        }
+    ],
+    comments: [
+        {
+            user: {
+                type: Schema.Types.ObjectId,
+                ref: 'users'
+            },
+            text: {
+                type: String,
+                required: true
+            },
+            name: {
+                type: String
+            },
+            avatar: {
+                type: String
+            },
+            date: {
+                type: Date,
+                default: Date.now
+            }
+        }
+    ],
+    date: {
+        type: Date,
+        default: Date.now
+    }
 });
 
 module.exports = Post = moogoose.model('posts', postSchema);
