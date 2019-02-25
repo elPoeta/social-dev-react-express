eb0aee6cc6b879767f5d5dccee3fb8aaa385aabe
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2019-02-25 12:39:14 -0300

    add create and delete comments

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -3,7 +3,7 @@ const Profile = require('../models/Profile');
 
 module.exports = {
     createPost: async (req, res, next) => {
-        const user = req.user._id;
+        const user = req.user.id;
         const { text, name, avatar } = req.body;
         const newPost = new Posts({
             text,
@@ -15,7 +15,7 @@ module.exports = {
             const post = await newPost.save();
             res.json(post);
         } catch (error) {
-            res.json({ post: 'No posts found' });
+            res.json({ postNotFound: 'No posts found' });
         }
 
     },
@@ -35,49 +35,99 @@ module.exports = {
             }
             res.json(post);
         } catch (error) {
-            res.json({ post: 'Post not found' });
+            res.json({ postNotFound: 'Post not found' });
 
         }
     },
     deletePost: async (req, res, next) => {
         try {
-            const profile = await Profile.findOne({ user: req.user._id });
-            const post = await Posts.findById({ _id: req.params._id });
-            if (post.user.toString() !== req.params._id) {
+            const profile = await Profile.findOne({ user: req.user.id });
+            if (!profile) {
+                return res.json('Post not found');
+            }
+            const post = await Posts.findById({ _id: req.params.id });
+            if (post.user.toString() !== req.params.id) {
                 return res.status(401).json({ noAuthorized: 'User no authorized' });
             }
             await post.remove();
             res.json({ success: true });
         } catch (error) {
-            res.json({ post: 'Post not found' });
+            res.json({ postNotFound: 'Post not found' });
         }
     },
     likePost: async (req, res, next) => {
         try {
-            const profile = await Profile.findOne({ user: req.user._id });
-            const post = await Posts.findById({ _id: req.params._id });
-            if (post.likes.filter(like => like.user.toString() === req.user._id).length > 0) {
+            const profile = await Profile.findOne({ user: req.user.id });
+            if (!profile) {
+                return res.json('Profile not found');
+            }
+            const post = await Posts.findById({ _id: req.params.id });
+            if (post.likes.filter(like => like.user.toString() === req.user.id).length > 0) {
                 return res.status(400).json({ alreadyLiked: 'User already liked this post' });
             }
-            post.like = [...post.like, req.user._id]
+            post.likes = [...post.likes, { user: req.user.id }];
+
             await post.save();
             res.json(post);
         } catch (error) {
-            res.json({ post: 'Post not found' });
+            res.json({ postNotFound: 'Post not found' });
         }
     },
     unLikePost: async (req, res, next) => {
+
         try {
-            const profile = await Profile.findOne({ user: req.user._id });
-            const post = await Posts.findById({ _id: req.params._id });
-            if (post.likes.filter(like => like.user.toString() === req.user._id).length === 0) {
+            const profile = await Profile.findOne({ user: req.user.id });
+            if (!profile) {
+                return res.json('Profile not found');
+            }
+            const post = await Posts.findById({ _id: req.params.id });
+
+            if (post.likes.filter(like => like.user.toString() === req.user.id).length === 0) {
+
                 return res.status(400).json({ notLiked: 'User not liked this post' });
             }
-            post.like = [...post.like.filter(like => like.user.toString() !== req.user._id)];
+
+            post.likes = [...post.likes.filter(like => like.user.toString() !== req.user.id)];
+
             await post.save();
+
             res.json(post);
         } catch (error) {
-            res.json({ post: 'Post not found' });
+            res.json({ postNotFound: 'Post not found' });
+        }
+    },
+    createComment: async (req, res, next) => {
+        const { text, name, avatar } = req.body;
+        const user = req.user.id;
+        try {
+            const post = await Posts.findById({ _id: req.params.id });
+            if (!post) {
+                return res.json({ postNotFound: 'Post not found' });
+            }
+            const newComment = { text, name, avatar, user };
+            post.comments = [...post.comments, newComment];
+            await post.save();
+            res.json(post);
+
+        } catch (error) {
+            res.json({ postNotFound: 'Post not found' });
+        }
+    },
+    deleteComment: async (req, res, next) => {
+        try {
+            const post = await Posts.findById({ _id: req.params.id });
+            if (!post) {
+                return res.json({ postNotFound: 'Post not found' });
+            }
+            if (post.comments.filter(comment => comment._id.toString() === req.params.comment_id).length === 0) {
+                return res.status(404).json({ commentNotExist: 'Comment not exist' });
+            }
+            post.comments = [...post.comments.filter(comment => comment._id.toString() !== req.params.comment_id)];
+            await post.save();
+            res.json(post);
+
+        } catch (error) {
+            res.json({ postNotFound: 'Post not found' });
         }
     }
 }
--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { createPost, getPosts, getPostById, deletePost, likePost, unLikePost } = require('../../controllers/posts');
+const { createPost, getPosts, getPostById,
+    deletePost, likePost, unLikePost,
+    createComment, deleteComment } = require('../../controllers/posts');
 const passport = require('passport');
 require('../../services/passport');
 const requireAuth = passport.authenticate('jwt', { session: false });
@@ -11,5 +13,7 @@ router.get('/:id', getPostById);
 router.delete('/:id', requireAuth, deletePost);
 router.post('/like/:id', requireAuth, likePost);
 router.post('/unlike/:id', requireAuth, unLikePost);
+router.post('/comment/:id', requireAuth, createComment);
+router.delete('/comment/:id/:comment_id', requireAuth, deleteComment);
 
 module.exports = router;
