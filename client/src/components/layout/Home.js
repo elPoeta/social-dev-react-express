b6c29b426db013fc4771f74d8062c4d90c93ca95
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2019-02-25 17:11:11 -0300

    start with signup and login

--- client/src/components/layout/Home.js
+++ client/src/components/layout/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import './Home.css'
 class Home extends Component {
     render() {
@@ -8,8 +9,8 @@ class Home extends Component {
                     <h2>Welcome! Social-Dev's</h2>
                     <p>Expand your knowledge...</p>
                     <ul>
-                        <li><button>Sign Up</button></li>
-                        <li><button>Login</button></li>
+                        <li><Link to='/signup'>Sign Up</Link></li>
+                        <li><Link to='/login'>Login</Link></li>
                     </ul>
                 </div>
 
--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,8 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import App from './components/App';
 import Home from './components/layout/Home';
+import SignUp from './components/auth/SignUp';
+import Login from './components/auth/Login';
 
 import './index.css';
 import * as serviceWorker from './serviceWorker';
@@ -12,6 +14,8 @@ ReactDOM.render(<BrowserRouter>
     <App>
         <Switch>
             <Route exact path='/' component={Home} />
+            <Route path='/signup' component={SignUp} />
+            <Route path='/login' component={Login} />
         </Switch>
     </App>
 </BrowserRouter>, document.querySelector('#root'));
